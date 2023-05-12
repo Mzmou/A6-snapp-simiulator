a6b1dd2004d486902679211fa88084b125fbd60a
marziyeh.mousavi <mzmousavi9@gmail.com>
2023-05-12 21:35:25 +0330

    I am bi asab

--- main.cpp
+++ main.cpp
@@ -77,34 +77,40 @@ class Driver
 {
 public:
     Driver(int id_) { driver_id = id_; }
-    int get_id(){return driver_id;}
-    private : 
+    int get_id() { return driver_id; }
+
+private:
     int driver_id;
     vector<Mission *> all_mission;
 };
+// template<class T>
 class DataBase
 {
 public:
     void add_count_mission(count_mission *c) { count_mission_data.push_back(c); }
     void add_time_mission(time_mission *c) { time_mission_data.push_back(c); }
     void add_distance_mission(distance_mission *c) { distance_mission_data.push_back(c); }
+    template <typename T>
+    void add_mission(T);
     void add_driver_id(int id);
     bool check_driver(int is);
     vector<count_mission *> get_count_mission_data() { return count_mission_data; }
+    vector<Mission *> total_mission;
 
 private:
     vector<count_mission *> count_mission_data;
     vector<distance_mission *> distance_mission_data;
     vector<time_mission *> time_mission_data;
-    friend ostream &operator<<(ostream &out, vector<count_mission *> count_mission_data);
+    // friend ostream &operator<<(ostream &out, vector<count_mission *> count_mission_data);
     vector<Driver *> Drivers_data;
 };
-bool DataBase::check_driver(int id)
+
+bool DataBase ::check_driver(int id)
 {
     for (int i = 0; i < Drivers_data.size(); i++)
     {
-        if (Drivers_data[i]->get_id()==id)
-        return true;
+        if (Drivers_data[i]->get_id() == id)
+            return true;
     }
     return false;
 }
@@ -112,6 +118,12 @@ void DataBase::add_driver_id(int id)
 {
     Drivers_data.push_back(new Driver(id));
 }
+template <typename T>
+void DataBase :: add_mission(T)
+{
+    total_mission.push_back(T());
+}
+
 Mission::Mission(string mission_id_str_form, string start_time_stamp_str_form, string end_time_stamp_str_form, int reward_)
 {
     mission_id = mission_id_str_form;
@@ -136,7 +148,7 @@ int main()
             try
             {
 
-                new_database.add_count_mission(new count_mission(mission_id, start_time_stamp, end_time_stamp, stoi(target_number), stoi(reward)));
+                new_database.add_mission(new count_mission(mission_id, start_time_stamp, end_time_stamp, stoi(target_number), stoi(reward)));
             }
             catch (string Error)
             {
@@ -151,7 +163,7 @@ int main()
             try
             {
 
-                new_database.add_time_mission(new time_mission(mission_id, start_time_stamp, end_time_stamp, stoi(time), stoi(reward)));
+                new_database.add_mission(new time_mission(mission_id, start_time_stamp, end_time_stamp, stoi(time), stoi(reward)));
             }
             catch (string Error)
             {
@@ -167,7 +179,7 @@ int main()
             try
             {
 
-                new_database.add_distance_mission(new distance_mission(mission_id, start_time_stamp, end_time_stamp, stoi(distance), stoi(reward)));
+                new_database.add_mission(new distance_mission(mission_id, start_time_stamp, end_time_stamp, stoi(distance), stoi(reward)));
             }
             catch (string Error)
             {
@@ -177,13 +189,15 @@ int main()
         }
 
         // vector<count_mission *> djkj = new_database.get_count_mission_data();
-        /*
+        
 
-        for (int i = 0; i < djkj.size(); i++)
+        for (int i = 0; i <new_database.total_mission.size(); i++)
         {
-            cout << djkj[i]->get_mission_id() << '\n';
+            cout <<new_database.total_mission[i]->get_mission_id() << '\n';
+            cout <<new_database.total_mission[i]->get_start_time_stamp() << '\n';
+
         }
-        */
+        
     }
 
     cout << "hi";
