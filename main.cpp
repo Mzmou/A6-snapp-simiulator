9d61a5fd43f335d129959b0740e7d1ab2a5b4f10
marziyeh.mousavi <mzmousavi9@gmail.com>
2023-05-13 16:34:59 +0330

    adding printig inheritance

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ public:
     int get_end_time_stamp() { return end_time_stamp; }
     void set_reward(int r) { reward = r; }
     Mission(string mission_id_str_form, string start_time_stamp_str_form, string end_time_stamp_str_form, int r);
+    Mission() { set_reward(0); }
+    virtual void print_mission() = 0;
 
 protected:
     string mission_id;
@@ -36,6 +38,7 @@ public:
         set_distance(distance_);
     }
     ~distance_mission();
+    virtual void print_mission();
 };
 class time_mission : public Mission
 {
@@ -53,7 +56,12 @@ public:
             throw INVALID_ARGUMENTS;
         set_time(time_);
     }
+    time_mission()
+    {
+        set_time(0);
+    }
     ~time_mission();
+    void virtual print_mission();
 };
 class count_mission : public Mission
 {
@@ -71,6 +79,7 @@ public:
             throw INVALID_ARGUMENTS;
         set_count(count_);
     }
+    virtual void print_mission();
     ~count_mission();
 };
 class Driver
@@ -90,8 +99,15 @@ public:
     void add_count_mission(count_mission *c) { count_mission_data.push_back(c); }
     void add_time_mission(time_mission *c) { time_mission_data.push_back(c); }
     void add_distance_mission(distance_mission *c) { distance_mission_data.push_back(c); }
+    bool assign_mission(int mission_id,int driver_id);
     template <typename T>
-    void add_mission(T);
+    void add_mission()
+    {
+        string mission_id_, start_time_stamp_, end_time_stamp_, distance_, mission_quality_;
+        cin >> mission_id_ >> start_time_stamp_ >> end_time_stamp_ >> distance_ >> mission_quality_;
+        total_mission.push_back(new T(mission_id_, start_time_stamp_, end_time_stamp_, stoi(distance_), stoi(mission_quality_)));
+    }
+
     void add_driver_id(int id);
     bool check_driver(int is);
     vector<count_mission *> get_count_mission_data() { return count_mission_data; }
@@ -104,7 +120,18 @@ private:
     // friend ostream &operator<<(ostream &out, vector<count_mission *> count_mission_data);
     vector<Driver *> Drivers_data;
 };
-
+void distance_mission::print_mission()
+{
+    cout << mission_id << " " << start_time_stamp << " " << end_time_stamp << " " << distance << " " << reward << "\n";
+}
+void count_mission::print_mission()
+{
+    cout << mission_id << " " << start_time_stamp << " " << end_time_stamp << " " << count << " " << reward << "\n";
+}
+void time_mission::print_mission()
+{
+    cout << mission_id << " " << start_time_stamp << " " << end_time_stamp << " " << time << " " << reward << "\n";
+}
 bool DataBase ::check_driver(int id)
 {
     for (int i = 0; i < Drivers_data.size(); i++)
@@ -118,14 +145,22 @@ void DataBase::add_driver_id(int id)
 {
     Drivers_data.push_back(new Driver(id));
 }
+/*
+
+
 template <typename T>
-void DataBase :: add_mission(T)
+void DataBase ::add_mission()
 {
-    total_mission.push_back(T());
+    string mission_id_, start_time_stamp_, end_time_stamp_, distance_, mission_quality_;
+    cin >> mission_id_ >> start_time_stamp_ >> end_time_stamp_ >> distance_ >> mission_quality_;
+    total_mission.push_back(new T(mission_id_, start_time_stamp_, end_time_stamp_, distance_, mission_quality_));
 }
+*/
 
 Mission::Mission(string mission_id_str_form, string start_time_stamp_str_form, string end_time_stamp_str_form, int reward_)
 {
+    if (mission_id=="" || start_time_stamp_str_form=="" || end_time_stamp_str_form=="")
+    throw INVALID_ARGUMENTS;
     mission_id = mission_id_str_form;
     start_time_stamp = stol(start_time_stamp_str_form);
     end_time_stamp = stol(end_time_stamp_str_form);
@@ -143,12 +178,12 @@ int main()
 
         if (command == "add_count_mission")
         {
-            string mission_id, start_time_stamp, end_time_stamp, target_number, reward;
-            cin >> mission_id >> start_time_stamp >> end_time_stamp >> target_number >> reward;
+            // string mission_id, start_time_stamp, end_time_stamp, target_number, reward;
+            // cin >> mission_id >> start_time_stamp >> end_time_stamp >> target_number >> reward;
             try
             {
-
-                new_database.add_mission(new count_mission(mission_id, start_time_stamp, end_time_stamp, stoi(target_number), stoi(reward)));
+                new_database.add_mission<count_mission>();
+                // new_database.add_mission(new count_mission(mission_id, start_time_stamp, end_time_stamp, stoi(target_number), stoi(reward)));
             }
             catch (string Error)
             {
@@ -158,12 +193,13 @@ int main()
         }
         if (command == "add_time_mission")
         {
-            string mission_id, start_time_stamp, end_time_stamp, time, reward;
-            cin >> mission_id >> start_time_stamp >> end_time_stamp >> time >> reward;
+            // string mission_id, start_time_stamp, end_time_stamp, time, reward;
+            // cin >> mission_id >> start_time_stamp >> end_time_stamp >> time >> reward;
             try
             {
-
-                new_database.add_mission(new time_mission(mission_id, start_time_stamp, end_time_stamp, stoi(time), stoi(reward)));
+                // time_mission t;
+                new_database.add_mission<time_mission>();
+                // new_database.add_mission(new time_mission(mission_id, start_time_stamp, end_time_stamp, stoi(time), stoi(reward)));
             }
             catch (string Error)
             {
@@ -174,12 +210,12 @@ int main()
 
         if (command == "add_distance_mission")
         {
-            string mission_id, start_time_stamp, end_time_stamp, distance, reward;
-            cin >> mission_id >> start_time_stamp >> end_time_stamp >> distance >> reward;
+            // string mission_id_, start_time_stamp_, end_time_stamp_, distance_, reward_;
+            // cin >> mission_id_ >> start_time_stamp_ >> end_time_stamp_ >> distance_ >> reward_;
             try
             {
-
-                new_database.add_mission(new distance_mission(mission_id, start_time_stamp, end_time_stamp, stoi(distance), stoi(reward)));
+                new_database.add_mission<distance_mission>();
+                //  new_database.add_mission(new distance_mission(mission_id, start_time_stamp, end_time_stamp, stoi(distance), stoi(reward)));
             }
             catch (string Error)
             {
@@ -189,15 +225,11 @@ int main()
         }
 
         // vector<count_mission *> djkj = new_database.get_count_mission_data();
-        
 
-        for (int i = 0; i <new_database.total_mission.size(); i++)
+        for (int i = 0; i < new_database.total_mission.size(); i++)
         {
-            cout <<new_database.total_mission[i]->get_mission_id() << '\n';
-            cout <<new_database.total_mission[i]->get_start_time_stamp() << '\n';
-
+            new_database.total_mission[i]->print_mission();
         }
-        
     }
 
     cout << "hi";
